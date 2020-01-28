cf492357e79ca0369ff7252b92c2682dc807f197
blue <playbynumbers@gmail.com>
2020-01-28 22:59:39 +0000

    Add a placeholder for ServerCommandProxy.cs
    Replace this file with the contents of ServerCommandProxy.cs from the
    PaperCut install files to build correctly.

---
+++ PaperCut/ServerCommandProxy.cs
@@ -0,0 +1,11 @@
+using System;
+
+//	Replace this file with the contents of ServerCommandProxy.cs from the PaperCut
+//	install files `[install path]\server\examples\webservices\csharp` as described at
+//	https://www.papercut.com/kb/Main/AdministeringPaperCutWithPowerShell#overview
+
+public class ServerCommandProxy {
+	public ServerCommandProxy(string server, int port, string authToken) {
+		throw new NotImplementedException();
+	}
+}
