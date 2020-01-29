0d189006314e09686039f28090136ebf7ae14c22
blue <playbynumbers@gmail.com>
2020-01-29 18:41:56 +0000

    Basic example app
    Swap Example.cs for the version in PaperCut install folder to be any
    real use.
    
    Target netcoreapp3.0 as that's the best the Ubuntu CI images provide.

---
+++ Example/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //	Replace this file with the contents of Example.cs from the PaperCut
+            //	install files `[install path]\server\examples\webservices\csharp` as described at
+            //	https://www.papercut.com/kb/Main/AdministeringPaperCutWithPowerShell#overview
+
+            Console.WriteLine("Please replace me with a real PaperCut Example.cs!");
+            // throw new NotImplementedException();
+        }
+    }
+}
