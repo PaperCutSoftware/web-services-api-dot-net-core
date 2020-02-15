1ea18762f2060ee925c2622159a8298b268b7bab
blue <playbynumbers@gmail.com>
2020-02-15 14:46:37 +0000

    Put cmdlets in PaperCut.PSPaperCut namespace

--- PSPaperCut/PSPaperCut.cs
+++ PSPaperCut/PSPaperCut.cs
@@ -1,29 +1,32 @@
 using System.Management.Automation;
 
-[Cmdlet("Connect","PaperCutServer")]
-[OutputType(typeof(ServerCommandProxy))]
-public class ConnectPaperCutServerCommand : PSCmdlet
+namespace PaperCut.PSPaperCut
 {
-    [Parameter(
-        Mandatory = true,
-        Position = 0,
-        ValueFromPipeline = true,
-        ValueFromPipelineByPropertyName = true)]
-    public string Server { get; set; }
-
-    [Parameter(
-        Position = 1,
-        ValueFromPipelineByPropertyName = true)]
-    public int Port { get; set; } = 9191;
-
-    [Parameter(
-        Mandatory = true,
-        Position = 2,
-        ValueFromPipelineByPropertyName = true)]
-    public string AuthToken { get; set; }
-
-    protected override void ProcessRecord()
+    [Cmdlet("Connect","PaperCutServer")]
+    [OutputType(typeof(ServerCommandProxy))]
+    public class ConnectPaperCutServerCommand : PSCmdlet
     {
-        WriteObject(new ServerCommandProxy(Server, Port, AuthToken));
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string Server { get; set; }
+
+        [Parameter(
+            Position = 1,
+            ValueFromPipelineByPropertyName = true)]
+        public int Port { get; set; } = 9191;
+
+        [Parameter(
+            Mandatory = true,
+            Position = 2,
+            ValueFromPipelineByPropertyName = true)]
+        public string AuthToken { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(new ServerCommandProxy(Server, Port, AuthToken));
+        }
     }
 }
